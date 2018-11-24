bd5f23b0f027de4888235f7aa9ceeac0904d4d69
g-ch <947089399@qq.com>
2018-11-24 16:40:04 +0800

    v1

--- cnn_encoder_3_layers.py
+++ cnn_encoder_3_layers.py
@@ -13,13 +13,19 @@ encoder_para = {
     "kernel2": 3,
     "stride2": 1,  # do not change
     "channel2": 64,
+    "kernel3": 3,
+    "stride3": 1,  # do not change
+    "channel3": 64,
     "pool1": 4,  # 2 or 4 recommended, carefully choose according to input_side_diamension to keep values interger
     "pool2": 4,  # 2 or 4 recommended, carefully choose according to input_side_diamension to keep values interger
+    "pool3": 2,  # 2 or 4 recommended, carefully choose according to input_side_diamension to keep values interger
     "outdia": 4096
 }
 
 pooled_side_len1 = input_side_diamension
 pooled_side_len2 = int(input_side_diamension / (encoder_para["pool1"]))
+pooled_side_len3 = int(input_side_diamension / (encoder_para["pool1"] * encoder_para["pool2"]))
+
 
 pooled_size = int(input_side_diamension * input_side_diamension * input_side_diamension * encoder_para["channel2"] / math.pow(encoder_para["pool1"]*encoder_para["pool2"], 3))
 
@@ -63,6 +69,11 @@ def encoder(x):
     d2 = encoder_para["channel2"]
     p2 = encoder_para["pool2"]
 
+    k3 = encoder_para["kernel3"]
+    s3 = encoder_para["stride3"]
+    d3 = encoder_para["channel3"]
+    p3 = encoder_para["pool3"]
+
     with tf.variable_scope("encoder"):
         with tf.variable_scope("conv1"):
             conv1 = conv3d_relu(x, [k1, k1, k1, 1, d1], [d1], [1, s1, s1, s1, 1])
@@ -71,8 +82,11 @@ def encoder(x):
         with tf.variable_scope("conv2"):
             conv2 = conv3d_relu(max_pool1, [k2, k2, k2, d1, d2], [d2], [1, s2, s2, s2, 1])
             max_pool2 = max_pool(conv2, [1, p2, p2, p2, 1], [1, p2, p2, p2, 1])
-            print "max_pool2 ", max_pool2
-            return max_pool2
+
+        with tf.variable_scope("conv3"):
+            conv3 = conv3d_relu(max_pool2, [k3, k3, k3, d2, d3], [d3], [1, s3, s3, s3, 1])
+            max_pool3 = max_pool(conv3, [1, p3, p3, p3, 1], [1, p3, p3, p3, 1])
+            return max_pool3
 
 
 def decoder(x, batch_size):
@@ -86,13 +100,25 @@ def decoder(x, batch_size):
     d1 = encoder_para["channel1"]
     p1 = encoder_para["pool1"]
 
+    k3 = encoder_para["kernel3"]
+    s3 = encoder_para["stride3"]
+    d3 = encoder_para["channel3"]
+    p3 = encoder_para["pool3"]
+
     pl1 = pooled_side_len1
     pl2 = pooled_side_len2
+    pl3 = pooled_side_len3
 
     with tf.variable_scope("decoder"):
+        with tf.variable_scope("conv0"):
+            conv0 = conv3d_relu(x, [k3, k3, k3, d3, d3], [d3], [1, 1, 1, 1, 1])
+            print "conv0 ", conv0
+
+        with tf.variable_scope("deconv0"):
+            deconv0 = deconv3d(conv0, [p3, p3, p3, d3, d3], output_shape=[batch_size, pl3, pl3, pl3, d3], strides=[1, p3, p3, p3, 1])
+            print "deconv0", deconv0.get_shape()
         with tf.variable_scope("conv1"):
-            conv1 = conv3d_relu(x, [k2, k2, k2, d2, d2], [d2], [1, 1, 1, 1, 1])
-            print "conv1 ", conv1
+            conv1 = conv3d_relu(deconv0, [k3, k3, k3, d3, d2], [d2], [1, s3, s3, s3, 1])
 
         with tf.variable_scope("deconv1"):
             deconv1 = deconv3d(conv1, [p2, p2, p2, d2, d2], output_shape=[batch_size, pl2, pl2, pl2, d2], strides=[1, p2, p2, p2, 1])
@@ -143,7 +169,6 @@ if __name__ == '__main__':
         for epoch in range(100):
             for i in range(total_batch):
                 sess.run(train_step, feed_dict={x_: data_mat})  # training
-
             print "epoch: " + str(epoch)
             print('loss=%s' % sess.run(loss, feed_dict={x_: data_mat}))
             if epoch % 10 == 0:
